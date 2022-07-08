67ef54f89a31c54b67ffc158bb0668de8b79c940
Nivedhya41 <nivedhya61@gmail.com>
2022-07-08 12:45:52 +0530

    gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPhotoComponent } from './add-photo/add-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPhotoComponent
   ],
   imports: [
     BrowserModule,
